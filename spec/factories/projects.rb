4af4e059db0eea3337134640354446d0f710cd02
matsuda.chikara <matsuda.chikara@elevennines.local>
2018-07-18 10:12:32 +0900

    4章終わった

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,8 +1,30 @@
 FactoryBot.define do
   factory :project do
-    sequence(:name) { |n| "Project #{n}" }
-    description 'A test project.'
+    sequence(:name) { |n| "Test Project #{n}" }
+    description 'Sample project for testing purposes'
     due_on 1.week.from_now
     association :owner
+
+    # 締め切りが昨日
+    trait :due_yesterday do
+      due_on 1.day.ago
+    end
+
+    # 締め切りが今日
+    trait :due_today do
+      due_on Date.current.in_time_zone
+    end
+
+    # 締め切りが明日
+    trait :due_tomorrow do
+      due_on 1.day.from_now
+    end
+
+    # メモ付きのプロジェクト
+    trait :with_notes do
+      after(:create) { |project| create_list(:note, 5, project: project) }
+    end
+
+    
   end
 end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -17,8 +17,6 @@ RSpec.describe Note, type: :model do
   # ファクトリで関連するデータを生成する
   it "generates associated data from a factory" do
     note = FactoryBot.create(:note)
-    puts "This note's project is #{note.project.inspect}"
-    puts "This note's user is #{note.user.inspect}"
   end
 
   # ユーザー、プロジェクト、メッセージがあれば有効な状態であること
