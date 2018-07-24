018f8d2bd5c9448503f5c83fdef40d27e5f1e81f
matsuda.chikara <matsuda.chikara@cospa-creation.co.jp>
2018-07-24 02:03:47 +0000

    8章終了

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'capybara', '~> 2.15.2'
   gem 'selenium-webdriver'
+  gem 'chromedriver-helper'
   gem 'rspec-rails', '~> 3.6.0'
   gem "factory_bot_rails", "~> 4.10.0"
   # gem 'launchy', '~> 2.4.3'
---
+++ spec/features/tasks_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.feature 'Tasks', type: :feature do
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) do
+    FactoryBot.create(:project,
+                      name: 'RSpec tutorial',
+                      owner: user)
+  end
+  let!(:task) { project.tasks.create!(name: 'Finish RSpec tutorial') }
+
+  # ユーザーがタスクの状態を切り替える
+  scenario 'user toggles a task', js: true do
+    login_as user, scope: :user
+    go_to_project 'RSpec tutorial'
+
+    complete_task 'Finish RSpec tutorial'
+    expect_complete_task 'Finish RSpec tutorial'
+
+    undo_complete_task 'Finish RSpec tutorial'
+    expect_incomplete_task 'Finish RSpec tutorial'
+  end
+
+  def go_to_project(name)
+    visit root_path
+    click_link name
+  end
+
+  def complete_task(name)
+    check name
+  end
+
+  def undo_complete_task(name)
+    uncheck name
+  end
+
+  def expect_complete_task(name)
+    aggregate_failures do
+      expect(page).to have_css 'label.completed', text: name
+      expect(task.reload).to be_completed
+    end
+  end
+
+  def expect_incomplete_task(name)
+    aggregate_failures do
+      expect(page).to_not have_css 'label.completed', text: name
+      expect(task.reload).to_not be_completed
+    end
+  end
+end
